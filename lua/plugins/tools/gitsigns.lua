3a3afd8b8ad49030b721d6db1f0f116a3ee81f91
Matthew Franchini <matchewf11@gmail.com>
2025-07-27 00:45:45 -0700

    Changed some keymap ideas

--- lua/plugins/tools/gitsigns.lua
+++ lua/plugins/tools/gitsigns.lua
@@ -1,7 +1,6 @@
 return {
-  'lewis6991/gitsigns.nvim', -- config later
+  'lewis6991/gitsigns.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
-  cmd = { 'Gitsigns' },
   opts = {
     signs = {
       add = { text = '┃' },
@@ -110,40 +109,67 @@ return {
       --   end
       -- end)
 
+      -- Navigation
+
       -- Actions
       map('n', '<leader>hp', gitsigns.preview_hunk, { desc = '[h]unk [p]review' })
       map('n', '<leader>hs', gitsigns.stage_hunk, { desc = '[h]unk [s]tage' })
+      map('n', '<leader>hr', gitsigns.reset_hunk, { desc = '[h]unk [r]eset' })
 
-      -- Actions
+      -- -- Navigation
+      -- map('n', ']c', function()
+      --   if vim.wo.diff then
+      --     vim.cmd.normal { ']c', bang = true }
+      --   else
+      --     gitsigns.nav_hunk 'next'
+      --   end
+      -- end)
+      --
+      -- map('n', '[c', function()
+      --   if vim.wo.diff then
+      --     vim.cmd.normal { '[c', bang = true }
+      --   else
+      --     gitsigns.nav_hunk 'prev'
+      --   end
+      -- end)
+      --
+      -- -- Actions
       -- map('n', '<leader>hs', gitsigns.stage_hunk)
       -- map('n', '<leader>hr', gitsigns.reset_hunk)
+      --
       -- map('v', '<leader>hs', function()
       --   gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
       -- end)
+      --
       -- map('v', '<leader>hr', function()
       --   gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
       -- end)
+      --
       -- map('n', '<leader>hS', gitsigns.stage_buffer)
       -- map('n', '<leader>hR', gitsigns.reset_buffer)
       -- map('n', '<leader>hp', gitsigns.preview_hunk)
       -- map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+      --
       -- map('n', '<leader>hb', function()
       --   gitsigns.blame_line { full = true }
       -- end)
+      --
       -- map('n', '<leader>hd', gitsigns.diffthis)
+      --
       -- map('n', '<leader>hD', function()
       --   gitsigns.diffthis '~'
       -- end)
+      --
       -- map('n', '<leader>hQ', function()
       --   gitsigns.setqflist 'all'
       -- end)
       -- map('n', '<leader>hq', gitsigns.setqflist)
-
-      -- Toggles
+      --
+      -- -- Toggles
       -- map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
       -- map('n', '<leader>tw', gitsigns.toggle_word_diff)
       --
-      -- Text object
+      -- -- Text object
       -- map({ 'o', 'x' }, 'ih', gitsigns.select_hunk)
     end,
   },
