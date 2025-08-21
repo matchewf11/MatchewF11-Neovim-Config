074bae5e3560fb99f799c8f55dbbe2bda86f4551
Matthew Franchini <matchewf11@gmail.com>
2025-08-21 13:29:44 -0700

    updated terminal mode -> normal mode map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,9 +8,8 @@ function M.global()
 
   -- NOTE:This won't work in all terminal emulators/tmux/etc.Try your own mapping
   -- or just use <C-\><C-n> to exit terminal mode
-  --vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+  vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
-  -- split navigation
   vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
   vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
   vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
